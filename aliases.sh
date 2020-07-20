7c0d3cb73ba4f5c74837e047abc96100fd4719f0
franco sanchez <franco_genta@hotmail.com>
2020-07-20 12:13:39 -0300

    add sh

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+[alias]
+	i = init
+	st = status
+	aa = add -A
+	ct = commit -m
+	co = checkout
+	lg = log
+	ph = push
+	pl = pull
+	sw = show
+	bh = branch
+	ce = clone
+	rt = reset
+	sch = switch
+	fch = fetch
+# Continuara...
